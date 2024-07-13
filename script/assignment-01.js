f3e8eb60be63745192138543959abcaa283cf272
transcenddev <97653516+transcenddev@users.noreply.github.com>
2024-07-13 11:06:22 +0800

    Learning about Variables

---
+++ script/assignment-01.js
@@ -0,0 +1,36 @@
+console.log('1. Add 2 numbers together! (just type console.log(23 + 97) into your HTML file)');
+
+console.log(23 + 97); // 120
+
+
+console.log('2. Add a sequence of different numbers');
+console.log(1 + 2 + 3 + 4 + 5);
+
+
+console.log('3. Declare a variable and then log it to the console');
+let a = 10;
+
+console.log(a);
+
+console.log('4. Try the follow 9 * a');
+
+console.log(9 * a);
+
+console.log('4.1 let b = 7 * a (returns undefined *) and then console.log(b)');
+let b = 7 * a;
+
+console.log(b);
+
+console.log('5 Declare a constant variable MAX with the value 57');
+
+const MAX = 57;
+console.log(MAX);
+
+console.log('Set another variable actual to MAX - 13');
+
+const actual = MAX - 13;
+console.log(actual);
+
+console.log('Set another variable percentage to actual / MAX');
+const percentage = actual / MAX;
+console.log(percentage);
